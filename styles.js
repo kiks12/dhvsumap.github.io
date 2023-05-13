ec2741ffc770d6e403689a738670a7da05237818
Francis James Tolentino <francistolentino1107@gmail.com>
2023-05-13 23:18:48 +0800

    initial commit

---
+++ styles.js
@@ -0,0 +1,50 @@
+const styles = {
+  'Office': {
+    fillColor: "#f0752e",
+    fillOpacity: 0.6,
+    color: "#f0752e",
+    weight: 0.5,
+  },
+  'Classroom': {
+    fillColor: "#3630e6",
+    fillOpacity: 0.6,
+    color: "#3630e6",
+    weight: 0.5,
+  },
+  'Facilities': {
+    fillColor: "#eb178c",
+    fillOpacity: 0.6,
+    color: "#eb178c",
+    weight: 0.5,
+  },
+  'Comfort Room': {
+    fillColor: "gray",
+    fillOpacity: 0.4,
+    color: "gray",
+    weight: 0.5,
+  },
+  'Grounds': {
+    fillColor: "#489943",
+    fillOpacity: 0.6,
+    weight: 0,
+    color: "#489943",
+  },
+  'International': {
+    fillColor: "#ebe017",
+    fillOpacity: 0.6,
+    weight: 0.5,
+    color: "#ebe017",
+  },
+  'Water': {
+    fillColor: "#8cd7ff",
+    fillOpacity: 0.6,
+    weight: 0.5,
+    color: "#8cd7ff",
+  },
+  'Default': {
+    fillColor: "#dbdbdb",
+    fillOpacity: 0.4,
+    weight: 0.5,
+    color: "#dbdbdb",
+  }
+};
